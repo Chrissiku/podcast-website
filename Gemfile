787456710679d86d757b26bc3f985f3405ec059f
Chris Siku <chrissiku5@gmail.com>
2023-03-02 19:12:50 +0200

    Add simple_form gem and install stimulus and simple_form for bootstrap

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :test do
 end
 
 gem "dotenv-rails", "~> 2.8"
+
+gem "simple_form", "~> 5.2"
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import PodcastController from "./podcast_controller"
+application.register("podcast", PodcastController)
